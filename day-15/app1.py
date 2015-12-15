d7ca3f5e48ca19c650545355d2af3b2d33929e73
Brent Chesny <brent.chesny@gmail.com>
2015-12-15 14:01:06 +0100

    Solved day 15

---
+++ day-15/app1.py
@@ -0,0 +1,19 @@
+import re, operator
+ingredients = [map(int, re.findall("(-?[\d]+)", line)[:-1]) for line in open("input.txt")]
+maxscore = 0
+
+def a(i, left, scores):
+	global maxscore
+
+	if i == len(ingredients):
+		score = reduce(operator.mul, [max(b,0) for b in scores])
+		if score > maxscore:
+			maxscore = score
+		return
+
+	for x in xrange(0,left + 1):
+		s = [z[0] + z[1] for z in zip(scores, [x*b for b in ingredients[i]])]
+		a(i+1, left-x, s) 
+
+a(0, 100, [0 for x in xrange(len(ingredients[0]))])
+print maxscore
