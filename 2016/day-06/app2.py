3c7047bb68b67881fcf6be6b2b310e95371ac452
Brent Chesny <brent.chesny@gmail.com>
2016-12-06 10:41:31 +0100

    Solved day 6

---
+++ 2016/day-06/app2.py
@@ -0,0 +1,8 @@
+from collections import Counter
+
+messages = [s.strip() for s in open('input.txt').readlines()]
+messages = zip(*messages)
+
+decoded = [Counter(s).most_common()[-1][0] for s in messages]
+
+print ''.join(decoded)
