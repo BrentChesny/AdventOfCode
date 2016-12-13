d13c9f8dc95c0116e9d95d10427d7a090fd82e66
Brent Chesny <brent.chesny@gmail.com>
2016-12-13 10:40:38 +0100

    Solved day 13

---
+++ 2016/day-13/app2.py
@@ -0,0 +1,25 @@
+puzzle_input = 1362
+
+directions = [(0,1), (0,-1), (1,0), (-1,0)]
+visited = {(1,1)}
+
+queue = [(0,1,1)]
+
+def valid(x,y):
+    n = x*x + 3*x + 2*x*y + y + y*y + puzzle_input
+    ones = bin(n).count('1')
+    return ones % 2 == 0
+
+while queue:
+    steps, x, y = queue.pop(0)
+
+    if steps == 50:
+        print len(visited)
+        break
+
+    for dx, dy in directions:
+        newx, newy = (x + dx, y + dy)
+
+        if newx >= 0 and newy >= 0 and valid(newx, newy) and (newx,newy) not in visited:
+            visited.add((newx,newy))
+            queue.append((steps + 1, newx, newy))
