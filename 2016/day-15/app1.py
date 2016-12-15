663311ce087de8d4d222b0edd2703439bffca7ca
Brent Chesny <brent.chesny@gmail.com>
2016-12-15 13:34:04 +0100

    Solved day 15

---
+++ 2016/day-15/app1.py
@@ -0,0 +1,23 @@
+import re
+
+discs = [map(int, re.match('Disc #(\d+) has (\d+) positions; at time=0, it is at position (\d+)', line).groups()) for line in open('input.txt').readlines()]
+
+time = 0
+
+while True:
+    time_found = True
+
+    for nr, size, pos in discs:
+        current_time = time + nr
+        current_pos = (pos + current_time) % size
+
+        if current_pos != 0:
+            time_found = False
+            break
+
+    if time_found:
+        break
+
+    time += 1
+
+print time
