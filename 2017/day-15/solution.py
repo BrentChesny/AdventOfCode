0db698fe4165d38da85bbb6d461e82abd0da478c
Brent Chesny <brent.chesny@gmail.com>
2017-12-15 23:30:48 +0100

    Modified code for day 15

--- 2017/day-15/solution.py
+++ 2017/day-15/solution.py
@@ -1,20 +1,6 @@
 def solve_part_one():
-    def gen_a():
-        prev = 277
-        while True:
-            prev *= 16807
-            prev %= 2147483647
-            yield prev
-
-    def gen_b():
-        prev = 349
-        while True:
-            prev *= 48271
-            prev %= 2147483647
-            yield prev
-
     total = 0
-    a, b = gen_a(), gen_b()
+    a, b = generator(277, 16807), generator(349, 48271)
     for _ in range(40000000):
         x = next(a)
         y = next(b)
@@ -23,30 +9,22 @@ def solve_part_one():
     return total
 
 def solve_part_two():
-    def gen_a():
-        prev = 277
-        while True:
-            prev *= 16807
-            prev %= 2147483647
-            if prev % 4 == 0:
-                yield prev
-
-    def gen_b():
-        prev = 349
-        while True:
-            prev *= 48271
-            prev %= 2147483647
-            if prev % 8 == 0:
-                yield prev
-
     total = 0
-    a, b = gen_a(), gen_b()
+    a, b = generator(277, 16807, 4), generator(349, 48271, 8)
     for _ in range(5000000):
         x = next(a)
         y = next(b)
         if ((x ^ y) & 0xFFFF) == 0:
             total += 1
     return total
+
+def generator(seed, factor, modulus=None):
+    prev = seed
+    while True:
+        prev *= factor
+        prev %= 2147483647
+        if not modulus or prev % modulus == 0:
+            yield prev
 
 def main():
     print 'Part one: ', solve_part_one()
